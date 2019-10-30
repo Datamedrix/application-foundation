3110dc1a16409927672510b19eb1f34d468415fc
Christian Graf <christian.graf@cyberfox.at>
2019-10-30 11:19:29 +0100

    refactor(database): Rebuild the migration and blueprint class to a simple wrapper for the classes from the dmx/support-toolset package.

--- src/Database/Schema/Blueprint.php
+++ src/Database/Schema/Blueprint.php
@@ -14,40 +14,8 @@ declare(strict_types=1);
 
 namespace DMX\Application\Database\Schema;
 
-use Illuminate\Support\Fluent;
-use Illuminate\Database\Query\Expression;
-use Illuminate\Database\Schema\Blueprint as BaseBlueprint;
+use DMX\Support\Database\Schema\Blueprint as BaseBlueprint;
 
 class Blueprint extends BaseBlueprint
 {
-    public const STRING_IDENTIFIER_LENGTH = 64;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function timestamps($precision = 0)
-    {
-        $this->timestamp('created_at', $precision)->default(new Expression('NOW()'));
-        $this->timestamp('updated_at', $precision)->nullable();
-    }
-
-    /**
-     * Add an identifier string (= VARCHAR(64) with charset 'ascii') to the table.
-     *
-     * @param string $column Name of the designated column
-     *
-     * @return Fluent
-     */
-    public function stringIdentifier(string $column): Fluent
-    {
-        return $this->string($column, self::STRING_IDENTIFIER_LENGTH)->charset('ascii');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function uuid($column): Fluent
-    {
-        return parent::uuid($column)->unique('uk--uuid')->index('idx--uuid')->charset('ascii');
-    }
 }
